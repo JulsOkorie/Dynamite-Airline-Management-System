daa0bfd824dcb0c0f774c3ceb465d6738cf2257c
Juliet <Juliet@Julz>
2022-08-12 13:36:43 +0100

    My initial commit.

---
+++ src/airlinemanagementsystem/AirlineManagementSystem.java
@@ -0,0 +1,9 @@
+package airlinemanagementsystem;
+
+public class AirlineManagementSystem {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
